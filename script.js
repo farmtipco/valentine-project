8e027ba3306a1f79a964168c5ca1ab47adae9cb0
Farm <farmtipco@gmail.com>
2026-02-13 03:24:19 +0700

    add chat cute message

--- script.js
+++ script.js
@@ -32,3 +32,61 @@ noBtn.addEventListener("click", function () {
     noBtn.innerText = phrases[noCount - 1];
   }
 });
+
+//หลังกดเป็น
+let chatStep = 0;
+
+const messages = [
+  ["left", "ขอบคุณที่กดเป็นนะ เค้าดีใจมากๆเลย❤️"],
+  ["right", "ต้องกดอยู่แล้วสิ!"],
+  ["left", "ปีนี้ขอให้เราอยู่ด้วยกันไปนานๆเลย"],
+  ["right", "นานแค่ไหน?!"],
+  ["left", "เหลืออีกตั้ง 73 ฤดูร้อน,ฝน,หนาวแหนะ"],
+  ["left", "Happy Valentine’s Day;)💖"],
+  ["right", "❤️"]
+];
+
+image.addEventListener("click", function () {
+
+  // ถ้ายังไม่เข้าสู่โหมด chat
+  if (text.innerHTML === "YAYYY!!!! ❤️") {
+    showChatScreen();
+  }
+});
+
+function showChatScreen() {
+  document.body.innerHTML = `
+    <div class="phone-wrapper">
+      <img src="image/iphone.png" class="phone-frame">
+      <div class="chat-screen" id="chatScreen"></div>
+    </div>
+  `;
+
+  const chatScreen = document.getElementById("chatScreen");
+
+  document.addEventListener("click", function () {
+    if (chatStep < messages.length) {
+
+      const bubble = document.createElement("div");
+      bubble.classList.add("chat-bubble");
+
+      const side = messages[chatStep][0];
+      const messageText = messages[chatStep][1];
+
+      if (side === "left") {
+        bubble.classList.add("chat-left");
+      } else {
+        bubble.classList.add("chat-right");
+      }
+
+      bubble.innerText = messageText;
+
+      chatScreen.appendChild(bubble);
+      chatScreen.scrollTop = chatScreen.scrollHeight;
+
+      chatStep++;
+    }
+  });
+
+
+}
